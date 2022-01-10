f0693ad594d944b5a5919becd43542993c94adac
iaia <iaia72160@gmail.com>
2022-01-10 17:20:42 +0900

    First init

---
+++ core/src/commonMain/kotlin/dev/iaiabot/testkmmlibrary/Greeting.kt
@@ -0,0 +1,7 @@
+package dev.iaiabot.testkmmlibrary
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}!"
+    }
+}
---
+++ core/src/commonMain/kotlin/dev/iaiabot/testkmmlibrary/Platform.kt
@@ -0,0 +1,5 @@
+package dev.iaiabot.testkmmlibrary
+
+expect class Platform() {
+    val platform: String
+}
